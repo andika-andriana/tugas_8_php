08d31e1d21fd33bc8f6c25368cdd00af881fa45b
Andika Andriana <andriana.mia1@gmail.com>
2019-12-29 11:37:46 +0700

    Tugas 8 PHP
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas8.php
@@ -0,0 +1,351 @@
+<?php
+  echo("
+  <table>
+    <tbody>
+    <tr>
+      <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+    </td>
+    <td>
+  ");
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  for($i = 10; $i > 0; $i--){
+    for($j = 1; $j <= $i; $j++){
+      echo "_";
+    }for($k = 10; $k > $i; $k--){
+      echo"x";
+    }for($l = 10; $l > $i; $l--){
+      echo"x";
+    }echo"<br>";
+  }for($m = 0; $m <= 10; $m++){
+    for($n = 1; $n <= $m; $n++){
+      echo"_";
+    }for($o = 10; $o > $m; $o--){
+      echo"x";
+    }for($p = 10; $p > $m; $p--){
+      echo"x";
+    }echo"<br>";
+  }
+  echo ("
+      </td>
+      </tr>
+    </tbody>
+  </table>")
+?>
